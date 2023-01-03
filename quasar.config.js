3e2317a695a527cbee5ddf62c7ca0ab1d69bea42
DaywisonSilva <daywisonsilvasou@gmail.com>
2023-01-03 00:39:16 -0300

    fix: change language pack

--- quasar.config.js
+++ quasar.config.js
@@ -93,7 +93,7 @@ module.exports = configure( function (/* ctx */ ) {
       config: {},
 
       // iconSet: 'material-icons', // Quasar icon set
-      // lang: 'en-US', // Quasar language pack
+      lang: 'pt-BR', // Quasar language pack
 
       // For special cases outside of where the auto-import strategy can have an impact
       // (like functional components as one of the examples),
