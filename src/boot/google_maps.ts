0323dfb5fe4f8db66476828394504ad8c91b7e33
DaywisonSilva <daywisonsilvasou@gmail.com>
2023-01-03 00:36:31 -0300

    fix: change to use .env file

---
+++ src/boot/google_maps.ts
@@ -0,0 +1,14 @@
+import { boot } from 'quasar/wrappers';
+
+// "async" is optional;
+// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+export default boot(async (/* { app, router, ... } */) => {
+  // something to do
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_KEY;
+  const script = document.createElement('script');
+  script.async = true;
+  script.defer = true;
+  script.src = `https://maps.googleapis.com/maps/api/js?libraries=places&key=${apiKey}&language=pt-BR`;
+
+  document.head.appendChild(script);
+});
